4c7bbe2b5e67b0cb0293385c1c5725db168fcb1c
ElliVerano <everano@zoogtech.com>
2023-05-03 20:28:30 +0800

    EVENTS: Add routes to event details and event register

--- lib/modules/events/events.dart
+++ lib/modules/events/events.dart
@@ -1,7 +1,11 @@
 import 'package:butter/butter.dart';
 
 import 'pages/events_page.dart';
+import 'pages/event_details_page.dart';
+import 'pages/event_register_page.dart';
 import 'states/events_state.dart';
+import 'states/event_details_state.dart';
+import 'states/event_register_state.dart';
 
 class Events extends BaseModule {
   Events()
@@ -9,10 +13,22 @@ class Events extends BaseModule {
           routeName: '/events',
           routes: {
             // This is the root route of the module ('/').
-            '/': BasePageConnector<EventsState, EventsPage>(
+            '/_/events': BasePageConnector<EventsState, EventsPage>(
               state: EventsState(),
               page: EventsPage(),
               getPage: (vm) => EventsPage(model: vm.model),
+            ),
+            '/_/events/details':
+                BasePageConnector<EventDetailsState, EventDetailsPage>(
+              state: EventDetailsState(),
+              page: EventDetailsPage(),
+              getPage: (vm) => EventDetailsPage(model: vm.model),
+            ),
+            '/_/events/register':
+                BasePageConnector<EventRegisterState, EventRegisterPage>(
+              state: EventRegisterState(),
+              page: EventRegisterPage(),
+              getPage: (vm) => EventRegisterPage(model: vm.model),
             ),
           },
         );
