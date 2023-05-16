fc008a41a9f33506d453459690f4b9b93b7feac7
ElliVerano <everano@zoogtech.com>
2023-05-16 15:57:51 +0800

    PRAY: Add login prompt

--- lib/modules/pray/models/pray_model.dart
+++ lib/modules/pray/models/pray_model.dart
@@ -2,21 +2,28 @@ import 'package:butter/butter.dart';
 
 class PrayModel extends BaseUIModel<PrayModel> {
   Map? isToday;
+  bool isLoggedIn;
   late void Function(String route) showPage;
+  late void Function() checkIsLoggedIn;
 
   PrayModel({
     this.isToday,
+    this.isLoggedIn = false,
   });
 
   @override
   String get $key => '/pray';
 
   @override
-  PrayModel clone() => PrayModel(isToday: isToday);
+  PrayModel clone() => PrayModel(
+        isToday: isToday,
+        isLoggedIn: isLoggedIn,
+      );
 
   @override
   int get hashCode => Object.hashAll([
         isToday,
+        isLoggedIn,
       ]);
 
   @override
@@ -24,5 +31,6 @@ class PrayModel extends BaseUIModel<PrayModel> {
       identical(this, other) ||
       other is PrayModel &&
           runtimeType == other.runtimeType &&
-          isToday == other.isToday;
+          isToday == other.isToday &&
+          isLoggedIn == other.isLoggedIn;
 }
