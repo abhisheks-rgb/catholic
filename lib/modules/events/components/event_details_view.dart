4a94e769ab3bce0d2a3d953c9b83126e0887dad8
ElliVerano <everano@zoogtech.com>
2023-05-22 14:15:48 +0800

    EVENTS: Implement add event to users device calendar; Implement navigate to maps when clicking venue

--- lib/modules/events/components/event_details_view.dart
+++ lib/modules/events/components/event_details_view.dart
@@ -1,8 +1,10 @@
+import 'package:add_2_calendar/add_2_calendar.dart' as a2c;
 import 'package:butter/butter.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:html/parser.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../models/event_details_model.dart';
 import '../../../utils/asset_path.dart';
@@ -144,76 +146,124 @@ class _EventDetailsViewState extends State<EventDetailsView> {
                           ),
                         ),
                   const SizedBox(height: 16),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(
-                        width: 20,
-                        height: 20,
-                        child: Icon(
-                          FontAwesome5Solid.calendar,
-                          color: Color.fromRGBO(4, 26, 82, 0.5),
-                          size: 20,
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Start: ${_dateEventFormat(widget.model?.item!['startDate'])}',
-                            style: const TextStyle(
-                              color: Color.fromRGBO(4, 26, 82, 1),
-                              fontSize: 16,
-                            ),
+                  _dateEventFormat(widget.model?.item!['startDate']) == ''
+                      ? Container()
+                      : RawMaterialButton(
+                          constraints: const BoxConstraints(),
+                          materialTapTargetSize:
+                              MaterialTapTargetSize.shrinkWrap,
+                          onPressed: () {
+                            final startDate =
+                                _dateEvent(widget.model?.item!['startDate']);
+                            final endDate =
+                                _dateEvent(widget.model?.item!['endDate']);
+
+                            final a2c.Event event = a2c.Event(
+                              title: widget.model?.item!['eventName'] as String,
+                              description: _parseHtmlString(
+                                  widget.model?.item!['eventDescription']),
+                              location:
+                                  widget.model?.item!['eventVenue'] as String,
+                              startDate: startDate,
+                              endDate: endDate,
+                              allDay: false,
+                            );
+
+                            a2c.Add2Calendar.addEvent2Cal(event);
+                          },
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const SizedBox(
+                                    width: 20,
+                                    height: 20,
+                                    child: Icon(
+                                      FontAwesome5Solid.calendar,
+                                      color: Color.fromRGBO(4, 26, 82, 0.5),
+                                      size: 20,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 10),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Start: ${_dateEventFormat(widget.model?.item!['startDate'])}',
+                                        style: const TextStyle(
+                                          color: Color.fromRGBO(4, 26, 82, 1),
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 4),
+                                      Text(
+                                        'End: ${_dateEventFormat(widget.model?.item!['endDate'])}',
+                                        style: const TextStyle(
+                                          color: Color.fromRGBO(4, 26, 82, 1),
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
-                          const SizedBox(height: 4),
-                          Text(
-                            'End: ${_dateEventFormat(widget.model?.item!['endDate'])}',
-                            style: const TextStyle(
-                              color: Color.fromRGBO(4, 26, 82, 1),
-                              fontSize: 16,
-                            ),
+                        ),
+                  widget.model?.item!['eventVenue'] == ''
+                      ? const SizedBox(height: 8)
+                      : RawMaterialButton(
+                          constraints: const BoxConstraints(),
+                          materialTapTargetSize:
+                              MaterialTapTargetSize.shrinkWrap,
+                          onPressed: () {
+                            String query =
+                                widget.model?.item!['eventVenue'] as String;
+                            _redirectToMaps(query);
+                          },
+                          child: Column(
+                            children: [
+                              const SizedBox(height: 8),
+                              Row(
+                                children: [
+                                  const SizedBox(
+                                    width: 20,
+                                    height: 20,
+                                    child: Icon(
+                                      MaterialCommunityIcons.map_marker,
+                                      color: Color.fromRGBO(4, 26, 82, 0.5),
+                                      size: 20,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 10),
+                                  Expanded(
+                                    child: Text(
+                                      '${widget.model?.item!['eventVenue']}',
+                                      style: const TextStyle(
+                                        color: Color.fromRGBO(4, 26, 82, 1),
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(width: 10),
+                                  const SizedBox(
+                                    width: 24,
+                                    height: 24,
+                                    child: Icon(
+                                      MaterialCommunityIcons.directions,
+                                      color: Color.fromRGBO(12, 72, 224, 1),
+                                      size: 24,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 8),
+                            ],
                           ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 8),
-                  Row(
-                    children: [
-                      const SizedBox(
-                        width: 20,
-                        height: 20,
-                        child: Icon(
-                          MaterialCommunityIcons.map_marker,
-                          color: Color.fromRGBO(4, 26, 82, 0.5),
-                          size: 20,
                         ),
-                      ),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: Text(
-                          '${widget.model?.item!['eventVenue']}',
-                          style: const TextStyle(
-                            color: Color.fromRGBO(4, 26, 82, 1),
-                            fontSize: 16,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      const SizedBox(
-                        width: 24,
-                        height: 24,
-                        child: Icon(
-                          MaterialCommunityIcons.directions,
-                          color: Color.fromRGBO(12, 72, 224, 1),
-                          size: 24,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 8),
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -294,8 +344,16 @@ class _EventDetailsViewState extends State<EventDetailsView> {
     );
   }
 
+  DateTime _dateEvent(dateEvent) {
+    DateTime date =
+        DateTime.fromMillisecondsSinceEpoch(dateEvent['_seconds'] * 1000);
+
+    return date;
+  }
+
   String _dateEventFormat(dateEvent) {
-    DateTime date = DateTime(dateEvent['_seconds']);
+    DateTime date =
+        DateTime.fromMillisecondsSinceEpoch(dateEvent['_seconds'] * 1000);
     String formattedDate = DateFormat('E, d MMM yy - h:mma').format(date);
 
     return formattedDate;
@@ -313,5 +371,34 @@ class _EventDetailsViewState extends State<EventDetailsView> {
         parse(document.body?.text).documentElement!.text;
 
     return parsedString;
+  }
+
+  void _redirectToMaps(String query) async {
+    final googleMaps = 'https://www.google.com/maps/search/?api=1&query=$query';
+    final uri = Uri.parse(googleMaps);
+    urlLauncher(uri, 'web');
+  }
+
+  void urlLauncher(Uri uri, String source) async {
+    bool canLaunch = false;
+
+    if (await canLaunchUrl(uri)) {
+      canLaunch = true;
+    } else {
+      throw 'Could not launch $uri';
+    }
+
+    if (canLaunch) {
+      switch (source) {
+        case 'web':
+          await launchUrl(
+            uri,
+            mode: LaunchMode.externalApplication,
+          );
+          break;
+        default:
+          await launchUrl(uri);
+      }
+    }
   }
 }
