dc01e14c572b35f2cdb1548f424e87451ce1877c
Jeyner Gil Caga <jeynergilcaga@gmail.com>
2023-07-20 18:31:05 +0800

    chore(release): Release v2.1.51+163

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:butter/butter.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
@@ -35,6 +36,7 @@ void main() async {
 
   // await FirebaseService.initialize(store);
 
+  await _initFirebase(store);
   // Firebase crashlytics
   // Pass all uncaught "fatal" errors from the framework to Crashlytics
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
@@ -46,6 +48,71 @@ void main() async {
   };
 
   run(store, navigatorKey);
+}
+
+void _handleMessage(RemoteMessage? message) {
+  if (message == null) return;
+
+  String? title = message.notification!.title;
+  String? body = message.notification!.body;
+
+  Butter.d('**************$title');
+  Butter.d('**************$body');
+  // navigatorKey.currentState?.pushNamed(NotificationPage.route,
+  //     arguments: {'title': title, 'body': body});
+  // navigatorKey.currentState?.pushNamed('/_/notification', arguments: 'notif');
+  // NavigateAction.pushNamed('/_/notification');
+
+  // FirebaseService._store.dispatch(NotifReceivedAction());
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  Butter.d('Handling a background message ${message.messageId}');
+  // final navigatorKey = GlobalKey<NavigatorState>();
+  // navigatorKey.currentState
+  //     ?.pushNamed('/_/notification', arguments: 'notification');
+  // NavigateAction.pushNamed('/_/notification', arguments: {});
+}
+
+Future<void> _initFirebase(Store<AppState> store) async {
+// Push Notification
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    Butter.d('User granted permission');
+  } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+    Butter.d('User granted provisional permission');
+  } else {
+    Butter.d('User declined or has not accepted permission');
+  }
+
+  final token = await FirebaseMessaging.instance.getToken();
+
+  Butter.d('My token is $token');
+  // saveToken(token!);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true, badge: true, sound: true);
+
+  final msg = await FirebaseMessaging.instance.getInitialMessage();
+  _handleMessage(msg);
+
+  FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+  FirebaseMessaging.onMessage.listen(_handleMessage);
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 }
 
 void run(Store<AppState> store, GlobalKey<NavigatorState> navigatorKey) =>
